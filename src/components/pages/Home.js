919f5c13f6affd6794e247b519a6146309654a50
DavidBlueLamassu <dra21@cam.ac.uk>
2023-05-31 15:17:45 +0100

    Navbar Responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,69 @@ function Home() {
   return (
     <div>
       <h1 className="bg-red-600 pl-2">Home Page</h1>
+      <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
+        <p className="text-lg text-justify md:mr-20 mx-4">
+        Welcome to my webpage. I am currently training as a web developer and am in the final stages of a coding bootcamp in Front 
+        End Web Development (week 13 of 16) run by Trilogy Education Services. The skills that I learned over the course of this 
+        programme include coding with: React.js/JSX, Node.js, JavaScript/jQuery, CSS, and HTML (including Bootstrap). To date I 
+        have deployed eleven webpages on GitHub pages including a group project using Words API and Giphy API. I particularly 
+        enjoy the problem-solving aspects of working with JavaScript and its offshoots. Examples of my work may be found in the 
+        Portfolio section of this website. Please feel free to contact me. Social media links and my CV may be found in the 
+        Contact section.
+        </p>
     </div>
   );
 }
